94c27e698fdf48a6800e8885a25d26cdd794f47c
SugarSD <48305404+SugarSD@users.noreply.github.com>
2024-10-22 18:31:23 -0600

    Update Footer.js

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -8,7 +8,6 @@ function Footer() {
                 <p><a href="https://github.com/SugarSD" className="copyright">&copy;SugarSD 2023</a> All Rights Reserved</p>
                 <div className="footer-links">
                     <a href="https://github.com/SugarSD">Github</a>
-                    <a href="https://www.linkedin.com/in/joshua-rice-1642ab253/">LinkedIn</a>
                 </div>
             </div>
         </footer>
